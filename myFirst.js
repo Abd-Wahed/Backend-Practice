f77ae60d8eed76ef139fad07bd33403bc66a6380
Abdul wahed <mohammadwahed49@gmail.com>
2025-10-31 20:55:31 +0600

    express

--- myFirst.js
+++ myFirst.js
@@ -1,28 +1,45 @@
-let http =require("http")
+// When using nodeJs
 
-let server = http.createServer((req, res) => {
-   let obj;
-  if (req.url == "/news") {
-   obj = {
-      status: 1,
-      data: [{
-        newsTitle: 'ws',
-        newsDes: "hello newsletter"
-      },
-      {
-        newsTitle: 'IIP',
-        newsDes: "IIP sfkjie"
-      }
-      ]
-    }
-  }
-  res.end(JSON.stringify(obj))
-  if (req.url == "/about") {
 
-  }
-  if (req.url == "/") {
+// let http =require("http")
 
-  }
+// let server = http.createServer((req, res) => {
+//    let obj;
+//   if (req.url == "/news") {
+//    obj = {
+//       status: 1,
+//       data: [{
+//         newsTitle: 'ws',
+//         newsDes: "hello newsletter"
+//       },
+//       {
+//         newsTitle: 'IIP',
+//         newsDes: "IIP sfkjie"
+//       }
+//       ]
+//     }
+//   }
+//   res.end(JSON.stringify(obj))
+//   if (req.url == "/about") {
+
+//   }
+//   if (req.url == "/") {
+
+//   }
+// })
+
+// server.listen("8081")
+
+
+
+// when using express
+
+let express= require("express")
+
+let app = express()
+
+app.get("/",(req,res)=>{
+  res.send({status:1,msg:"Home page API"})
 })
 
-server.listen("8081")
+app.listen("8000")
