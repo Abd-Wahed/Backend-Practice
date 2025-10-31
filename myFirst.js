055ce042011484fc2a24a39d76c700d775dbf2ff
Abdul wahed <mohammadwahed49@gmail.com>
2025-10-31 23:58:11 +0600

    express

--- myFirst.js
+++ myFirst.js
@@ -34,12 +34,52 @@
 
 // when using express
 
-let express= require("express")
+let express = require("express")
 
 let app = express()
+app.use(express.json())//frontend theke backend data patabe
 
-app.get("/",(req,res)=>{
-  res.send({status:1,msg:"Home page API"})
+let checkToken=(req,res,next)=>{
+  console.log("Welcome")
+  next();
+}
+app.use(checkToken)
+
+app.get("/", (req, res) => {
+  res.send({ status: 1, msg: "Home page API" })
 })
 
+app.get("/news", (req, res) => {
+  res.send({
+    status: 2,
+    msg: "hello,i am from the news portal"
+  })
+})
+
+app.get("/news/:id", (req, res) => {
+  let currentId = req.params.id
+  res.send("News Details API" + currentId)
+})
+
+app.post("/login", (req, res) => {
+  console.log(req.body)
+
+  res.status(200).json(
+      {
+      status: 1,
+      msg: "Hello i am from login page",
+      data: req.body,
+      queryData:req.query
+    }
+  )
+
+  // res.send(
+  //   {
+  //     status: 1,
+  //     msg: "Hello i am from login page",
+  //     data: req.body,
+  //     queryData:req.query
+  //   }
+  // )
+})
 app.listen("8000")
