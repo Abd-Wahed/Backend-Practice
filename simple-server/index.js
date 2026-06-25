ad4207e5aec5c163ff7b1666f762b800303f743e
Abdul wahed <mohammadwahed49@gmail.com>
2026-06-26 04:08:04 +0600

    added simple server code

---
+++ simple-server/index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const app=express();
+
+app.use(cors());
+app.use(express.json())
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://mohammadwahed49_db_user:scGChrHiDfxrSmXN@cluster0.aohahqw.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db("usersDB");
+    const userCollection = database.collection("users");
+
+    app.post('/user',async(req,res)=>{
+       const user = req.body;
+       console.log('new user',user);
+       const result = await userCollection.insertOne(user);
+       res.send(result);
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (error) {
+    console.error(error);
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/' ,(req,res)=>{
+    res.send('simple crud ');
+});
+
+app.listen(port,()=>{
+    console.log(`simple crud is running on port : ${port}`);
+})
+// mohammadwahed49_db_user
+// scGChrHiDfxrSmXN
